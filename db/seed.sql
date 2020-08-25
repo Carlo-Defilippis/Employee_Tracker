eee3bd1ee711de28a59f512ef3cc1b285b5ba128
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-08-25 14:41:52 -0400

    Most questions working

--- db/seed.sql
+++ db/seed.sql
@@ -1,27 +1,33 @@
-USE employees;
--- department seeds
-INSERT INTO department 
+USE employee_trackerDB;
+-- add departments
+INSERT INTO department
     (name)
 VALUES
-    ("Sales"),
-    ("Engineering"),
-    ("HR"),
-    ("IT");
-    INSERT INTO role
-        (title, salary, department_id)
-    VALUES
-        ("Sales rep", 35000, 1),
-        ("Mechanical Engineer", 120000, 2),
-        ("Software Engineer", 150000, 2),
-        ("Jr Engineer", 80000, 2),
-        ("HR", 85000, 3),
-        ("IT", 70000, 4);
-        INSERT INTO employee
-            (first_name, last_name, role_id, manager_id)
-        VALUES
-        ("John", "Smith", 1, 1),
-        ("Manoli", "K", 2, 2),
-        ("Anne", "Foo", 2, NULL),
-        ("Thomas", "Holland", 2, NULL),
-        ("Kyle", "Andie", 3, 3),
-        ("Ed", "Gibbons", 4, 4);
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+-- add roles into corressponding departments
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+-- add the names for the employees
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { prompt } = require("inquirer");
+const inquirer = require("inquirer");
 const logo = require("asciiart-logo");
 const db = require("./db");
 const { inherits } = require("util");
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "6822",
-    database: "employees"
+    database: "employee_trackerDB"
 });
 
 connection.connect();
@@ -31,151 +31,413 @@ function init() {
 
 init(); 
 
-async function loadMainPrompts() {
-  const { choice } = await prompt([
-    {
-      type: "list",
-      name: "choice",
-      message: "What would you like to do?",
-      choices: [
-        {
-          name: "View all employees",
-          value: "VIEW_EMPLOYEES",
-        },
-        {
-          name: "View all employees by department",
-          value: "VIEW_EMPLOYEES_BY_DEPARTMENT",
-        },
-        {
-          name: "View all employees by manager",
-          value: "VIEW_EMPLOYEES_BY_MANAGER",
-        },
-        {
-          name: "Add employee",
-          value: "ADD_EMPLOYEE",
-        },
-        {
-          name: "Remove employee",
-          value: "REMOVE_EMPLOYEE",
-        },
-        {
-          name: "Update employee role",
-          value: "UPDATE_EMPLOYEE_ROLE",
-        },
-        {
-          name: "Update employee manager",
-          value: "UPDATE_EMPLOYEE_MANAGER",
-        },
-        {
-          name: "View all roles",
-          value: "VIEW_ROLES",
-        },
-        {
-          name: "Add role",
-          value: "ADD_ROLE",
-        },
-        {
-          name: "Remove role",
-          value: "REMOVE_ROLE",
-        },
-        {
-          name: "View all departments",
-          value: "VIEW_DEPARTMENTS",
-        },
-        {
-          name: "Add department",
-          value: "ADD_DEPARTMENT",
-        },
-        {
-          name: "Remove department",
-          value: "REMOVE_DEPARTMENTS",
-        },
-        {
-          name: "Quit",
-          value: "QUIT",
-        }
-      ]
-    }
-  ]);
-  
-  switch (choice) {
-    case "VIEW_EMPLOYEES":
-      return viewEmployees();
-    case "VIEW_EMPLOYEES_BY_DEPARTMENT":
-      return viewEmployeesByDepartment();
-    case "VIEW_EMPLOYEES_BY_MANAGER":
-      return viewEmployeesByManager();
-    case "ADD_EMPLOYEE":
-      return addEmployee();
-    case "REMOVE_EMPLOYEE":
-  return removeEmployee();
-  case "UPDATE_EMPLOYEE_ROLE":
-      return updateEmployeeRole();
-    case "VIEW_DEPARTMENT":
-      return viewDepartments();
-    case "ADD_DEPARTMENT":
-      return addDepartment();
-    case "REMOVE_DEPARTMENT":
-      return removeDepartment();
-    case "VIEW_ROLES":
-      return viewRoles();
-    case "ADD_ROLE":
-      return addRole();
-    case "REMOVE_ROLE":
-      return removeRole();
-    default:
-      return quit();
-  }
-}
+// async function loadMainPrompts() {
+//   const { choice } = await prompt([
+//     {
+//       type: "list",
+//       name: "choice",
+//       message: "What would you like to do?",
+//       choices: [
+//         {
+//           name: "View all employees",
+//           value: "VIEW_EMPLOYEES",
+//         },
+//         {
+//           name: "View all employees by department",
+//           value: "VIEW_EMPLOYEES_BY_DEPARTMENT",
+//         },
+//         {
+//           name: "View all employees by manager",
+//           value: "VIEW_EMPLOYEES_BY_MANAGER",
+//         },
+//         {
+//           name: "Add employee",
+//           value: "ADD_EMPLOYEE",
+//         },
+//         {
+//           name: "Remove employee",
+//           value: "REMOVE_EMPLOYEE",
+//         },
+//         {
+//           name: "Update employee role",
+//           value: "UPDATE_EMPLOYEE_ROLE",
+//         },
+//         {
+//           name: "Update employee manager",
+//           value: "UPDATE_EMPLOYEE_MANAGER",
+//         },
+//         {
+//           name: "View all roles",
+//           value: "VIEW_ROLES",
+//         },
+//         {
+//           name: "Add role",
+//           value: "ADD_ROLE",
+//         },
+//         {
+//           name: "Remove role",
+//           value: "REMOVE_ROLE",
+//         },
+//         {
+//           name: "View all departments",
+//           value: "VIEW_DEPARTMENTS",
+//         },
+//         {
+//           name: "Add department",
+//           value: "ADD_DEPARTMENT",
+//         },
+//         {
+//           name: "Remove department",
+//           value: "REMOVE_DEPARTMENTS",
+//         },
+//         {
+//           name: "Quit",
+//           value: "QUIT",
+//         }
+//       ]
+//     }
+//   ]);
+// }
+// //   switch (choice) {
+//     case "VIEW_EMPLOYEES":
+//       return viewEmployees();
+//     case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+//       return viewEmployeesByDepartment();
+//     case "VIEW_EMPLOYEES_BY_MANAGER":
+//       return viewEmployeesByManager();
+//     case "ADD_EMPLOYEE":
+//       return addEmployee();
+//     case "REMOVE_EMPLOYEE":
+//   return removeEmployee();
+//   case "UPDATE_EMPLOYEE_ROLE":
+//       return updateEmployeeRole();
+//     case "VIEW_DEPARTMENT":
+//       return viewDepartments();
+//     case "ADD_DEPARTMENT":
+//       return addDepartment();
+//     case "REMOVE_DEPARTMENT":
+//       return removeDepartment();
+//     case "VIEW_ROLES":
+//       return viewRoles();
+//     case "ADD_ROLE":
+//       return addRole();
+//     case "REMOVE_ROLE":
+//       return removeRole();
+//     default:
+//       return quit();
+//   }
+// }
 
-async function viewEmployees() {
-    var query = "SELECT first_name, last_name FROM employees.employee";
-    var employeeArr = [];
-    connection.query(query, function(err, res){
-        // var stringifyRes = JSON.stringify(res);
-        // console.log("stringifyRes: ", stringifyRes);
-        console.log(JSON.parse(JSON.stringify(res)));
-        let data = JSON.parse(JSON.stringify(res));
-        for (var i = 0; i < data.length; i++){
-            employeeArr.push(data[i].first_name + " " + data[i].last_name);
-        }
-        console.table("Employees", employeeArr);
-        loadMainPrompts();
-    });
+// async function viewEmployees() {
+//     var query = "SELECT first_name, last_name FROM employees.employee";
+//     var employeeArr = [];
+//     connection.query(query, function(err, res){
+//         // var stringifyRes = JSON.stringify(res);
+//         // console.log("stringifyRes: ", stringifyRes);
+//         console.log(JSON.parse(JSON.stringify(res)));
+//         let data = JSON.parse(JSON.stringify(res));
+//         for (var i = 0; i < data.length; i++){
+//             employeeArr.push(data[i].first_name + " " + data[i].last_name);
+//         }
+//         console.table("Employees", employeeArr);
+//         loadMainPrompts();
+//     });
     
+// }
+
+// async function viewEmployeesByDepartment() {
+//     const departments = await db.findAllDepartments();
+
+//     const departmentChoices = departments.map(( {id, name} ) => ({
+//         name: name,
+//         value: id
+//     }));
+// }
+
+// function addRole() {
+//     var dept = "SELECT name FROM department";
+//     connection.query(dept, function(err,res) {
+//         let data = JSON.parse(JSON.stringify(res));
+//         let departmentArr = [];
+//         for(var i = 0; i < data.length; i++){
+//             departmentArr.push(data[i].name);
+//         }
+//         console.log("dept Arr: ", departmentArr);
+//         inquirer
+//             .prompt([
+//                 {
+//                     type: "list",
+//                     message: "Choose the department to add the role too: ",
+//                     choices: departmentArr,
+//                     name: "department"
+//                 }
+//             ]);
+//             loadMainPrompts();
+//     });
+// }
+
+// function quit() {
+//     process.exit(0);
+// }
+
+// Dependcies
+
+async function loadMainPrompts() {
+  const answer = await inquirer.prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          {
+            name: "View all employees",
+            value: "VIEW_EMPLOYEES",
+          },
+          {
+            name: "View all employees by department",
+            value: "VIEW_EMPLOYEES_BY_DEPARTMENT",
+          },
+          {
+            name: "View all employees by manager",
+            value: "VIEW_EMPLOYEES_BY_MANAGER",
+          },
+          {
+            name: "Add employee",
+            value: "ADD_EMPLOYEE",
+          },
+          {
+            name: "Remove employee",
+            value: "REMOVE_EMPLOYEE",
+          },
+          {
+            name: "Update employee role",
+            value: "UPDATE_EMPLOYEE_ROLE",
+          },
+          {
+            name: "Update employee manager",
+            value: "UPDATE_EMPLOYEE_MANAGER",
+          },
+          {
+            name: "View all roles",
+            value: "VIEW_ROLES",
+          },
+          {
+            name: "Add role",
+            value: "ADD_ROLE",
+          },
+          {
+            name: "Remove role",
+            value: "REMOVE_ROLE",
+          },
+          {
+            name: "View all departments",
+            value: "VIEW_DEPARTMENTS",
+          },
+          {
+            name: "Add department",
+            value: "ADD_DEPARTMENT",
+          },
+          {
+            name: "Remove department",
+            value: "REMOVE_DEPARTMENTS",
+          },
+          {
+            name: "Quit",
+            value: "QUIT",
+          }
+        ]
+      }
+    ]).then(function (answer) {
+    switch (answer.choice) {
+      case "VIEW_EMPLOYEES":
+        return viewEmployees();
+      case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+        return viewEmployeesByDepartment();
+      case "VIEW_EMPLOYEES_BY_MANAGER":
+        return viewEmployeesByManager();
+      case "ADD_EMPLOYEE":
+        return addEmployee();
+      case "REMOVE_EMPLOYEE":
+        return removeEmployee();
+      case "UPDATE_EMPLOYEE_ROLE":
+        return updateEmployeeRole();
+      case "VIEW_DEPARTMENT":
+        return viewDepartments();
+      case "ADD_DEPARTMENT":
+        return addDepartment();
+      case "REMOVE_DEPARTMENT":
+        return removeDepartment();
+      case "VIEW_ROLES":
+        return viewRole();
+      case "ADD_ROLE":
+        return addRole();
+      case "REMOVE_ROLE":
+        return removeRole();
+      default:
+        return quit();
+    }
+});
+}
+// view all departments
+function viewDepartments() {
+  var query = "SELECT * FROM department ORDER BY id";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    // console.log("all departments: ", res);
+    console.table(res);
+    console.log("============================================================");
+    loadMainPrompts();
+  });
+}
+// view employees with their role title, role salary, and dept_name
+function viewEmployees() {
+  var query =
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.dept_name FROM employee INNER JOIN role ON role_id=role.id INNER JOIN department ON department.id=role.department_id;";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    // console.log("all employees: ", res);
+    console.table(res);
+    console.log("============================================================");
+    loadMainPrompts();
+  });
+}
+// view roles combined with the department name
+function viewRole() {
+  var query =
+    "SELECT department.dept_name, role.title, role.salary FROM department INNER JOIN role ON department.id=role.department_id ORDER BY dept_name";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    // console.log("all roles: ", res);
+    console.table(res);
+    console.log("============================================================");
+    loadMainPrompts();
+  });
 }
 
-async function viewEmployeesByDepartment() {
-    const departments = await db.findAllDepartments();
-
-    const departmentChoices = departments.map(( {id, name} ) => ({
-        name: name,
-        value: id
-    }));
-}
-
-function addRole() {
-    var dept = "SELECT name FROM department";
-    connection.query(dept, function(err,res) {
-        let data = JSON.parse(JSON.stringify(res));
-        let departmentArr = [];
-        for(var i = 0; i < data.length; i++){
-            departmentArr.push(data[i].name);
-        }
-        console.log("dept Arr: ", departmentArr);
-        inquirer
-            .prompt([
-                {
-                    type: "list",
-                    message: "Choose the department to add the role too: ",
-                    choices: departmentArr,
-                    name: "department"
-                }
-            ]);
-            loadMainPrompts();
+// add a new department - must be unique
+function addDepartment() {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "What is the name of the department you want to add?",
+      name: "department",
+    })
+    .then(function (answer) {
+      var query = "INSERT INTO department SET ?";
+      connection.query(query, { dept_name: answer.department }, function (
+        err,
+        res
+      ) {
+        if (err) throw err;
+        console.log("added new department");
+        console.log(
+          "============================================================"
+        );
+        loadMainPrompts();
+      });
     });
 }
 
+// add a new employee with first and last name, and role title
+function addEmployee() {
+  var empRole = "SELECT * FROM role ORDER BY id";
+  connection.query(empRole, function (err, res) {
+    if (err) throw err;
+    var empArr = [];
+    for (var i = 0; i < res.length; i++) {
+      empArr.push(res[i].id + ": " + res[i].title);
+    }
+    console.log("empArr: ", empArr);
+    inquirer.prompt([
+        {
+          type: "input",
+          message: "What is employee's first name?",
+          name: "first",
+        },
+        {
+          type: "input",
+          message: "What is employee's last name?",
+          name: "last",
+        },
+        {
+          type: "list",
+          message: "What is employee's job title?",
+          choices: empArr,
+          name: "role",
+        },
+      ])
+      .then(function (answer) {
+        var query = "INSERT INTO employee SET ?";
+        connection.query(
+          query,
+          {
+            first_name: answer.first,
+            last_name: answer.last,
+            role_id: parseInt(answer.role.split("")),
+          },
+          function (err, res) {
+            if (err) throw err;
+            console.log("Added new employee");
+            console.log(
+              "============================================================"
+            );
+            loadMainPrompts();
+          }
+        );
+      });
+  });
+}
+
+// add a new role from the existing deptartments with base salary
+function addRole() {
+  var dept = "SELECT * FROM department ORDER BY id";
+  connection.query(dept, function (err, res) {
+    var deptArr = [];
+    for (var i = 0; i < res.length; i++) {
+      deptArr.push(res[i].id + ": " + res[i].dept_name);
+    }
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Choose the department to add the role too:",
+          choices: deptArr,
+          name: "department",
+        },
+        {
+          type: "input",
+          message: "What is title of the role you want to add?",
+          name: "role",
+        },
+        {
+          type: "number",
+          message: "What is the starting salary?",
+          name: "salary",
+        },
+      ])
+      .then(function (answer) {
+        console.log("answer: ", answer);
+        var query = "INSERT INTO role SET ?";
+        connection.query(
+          query,
+          {
+            title: answer.role,
+            salary: answer.salary,
+            department_id: parseInt(answer.department.split("")),
+          },
+          function (err, res) {
+            if (err) throw err;
+            console.log("Added new role");
+            console.log(
+              "============================================================"
+            );
+            loadMainPrompts();
+          }
+        );
+      });
+  });
+}
+// exit out of the application
 function quit() {
-    process.exit(0);
+  process.exit(0);
 }
